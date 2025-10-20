7b82c3fe16ab58c9604975eb524e2256984f9bbb
Austin Borg <austin.borg@students.mq.edu.au>
2025-10-20 12:10:21 +1100

    added roxygen documentation for bank dataset

---
+++ multiserverpkg/R/data.R
@@ -0,0 +1,14 @@
+#' Bank Customer Data
+#'
+#' A dataset of 100 customers’ arrival and service times.
+#'
+#' @format A data frame with 100 rows and 2 variables:
+#' \describe{
+#'   \item{arrival_time}{Time of arrival of each customer}
+#'   \item{service_time}{Duration of service for each customer}
+#' }
+#' @source Generated using a Poisson process
+"bank"
+
+
+
